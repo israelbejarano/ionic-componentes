ce43a164f4366a663f0a45990c5d0d306f621610
israel <israel.bejarano.a@gmail.com>
2019-08-13 11:57:21 +0200

    uso de listas para menu dinámico

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert Sheet',
+      redirectTo: '/alert'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
