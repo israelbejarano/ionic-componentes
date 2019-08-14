aabd607cbe906f73a7fbabea6b8be64a5ad4878d
israel <israel.bejarano.a@gmail.com>
2019-08-14 11:13:06 +0200

    modal + modal-info pages

---
+++ src/app/pages/modal-info/modal-info.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ModalInfoPage } from './modal-info.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ModalInfoPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ModalInfoPage]
+})
+export class ModalInfoPageModule {}
